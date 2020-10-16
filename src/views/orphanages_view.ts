c91ed6e2f0de0fbacc46208a911ba2f2fd878bf1
MARCUS <mviniciusflx@gmail.com>
2020-10-16 00:45:08 -0300

    validação

--- src/views/orphanages_view.ts
+++ src/views/orphanages_view.ts
@@ -3,7 +3,7 @@ import imagesView from './images_view';
 
 export default {
     render(orphanage: Orphanage){
-        return{
+        return{  
             id: orphanage.id,
             name: orphanage.name,
             latitude: orphanage.latitude,
